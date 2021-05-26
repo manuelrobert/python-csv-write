4cf648fc14926766bd71f6af0b75c4433625a17e
Manuel Robert <manuelrobz@outlook.com>
2021-05-26 15:14:15 +0530

    refactored

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import csv
 
-with open('players.csv', 'w', newline='') as file:
-    fieldnames = ['player_name', 'fide_rating']
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
+player = [
+    {'player_name': 'Magnus Carlsen', 'fide_rating': 2870},
+    {'player_name': 'Fabiano Caruana', 'fide_rating': 2822},
+    {'player_name': 'Ding Liren', 'fide_rating': 2801}
+]
 
+print(player[0].keys())
+
+with open('players.csv', 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=player[0].keys())
     writer.writeheader()
-    writer.writerow({'player_name': 'Magnus Carlsen', 'fide_rating': 2870})
-    writer.writerow({'player_name': 'Fabiano Caruana', 'fide_rating': 2822})
-    writer.writerow({'player_name': 'Ding Liren', 'fide_rating': 2801})
+    writer.writerows(player)
